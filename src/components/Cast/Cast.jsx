2cbfdbe8479f1844e6b3e2537e0fa041ed8ee4f3
Oleksii <al.dimitrash@gmail.com>
2023-08-17 15:41:26 +0300

    update

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,7 +1,6 @@
 import { fetchMovieCredits } from '../../services/Api';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-
 import {
   CastContainer,
   CastList,
